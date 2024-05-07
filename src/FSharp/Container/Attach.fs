e4963c4fe119a4072ada4a7de62dd8b7d55d2d9c
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-06 21:17:16 -0500

    Added default values for AttachBuilder operations
    The AttachBuilder class in the FSharp Container has been updated to include default values for its custom operations. Now, if no argument is provided when calling the Logs, Stderr, Stdin, Stdout, or Stream methods on an instance of AttachBuilder, these methods will automatically use 'true' as their second argument. This change simplifies usage and reduces potential errors from missing arguments.

--- src/FSharp/Container/Attach.fs
+++ src/FSharp/Container/Attach.fs
@@ -27,17 +27,32 @@ type AttachBuilder(id: string) =
     [<CustomOperation("logs")>]
     member inline _.Logs(attach, logs) = { attach with Logs = logs }
 
+    [<CustomOperation("logs")>]
+    member inline this.Logs(attach) = this.Logs(attach, true)
+
     [<CustomOperation("stderr")>]
     member inline _.Stderr(attach, stderr) = { attach with Stderr = stderr }
+
+    [<CustomOperation("stderr")>]
+    member inline this.Stderr(attach) = this.Stderr(attach, true)
 
     [<CustomOperation("stdin")>]
     member inline _.Stdin(attach, stdin) = { attach with Stdin = stdin }
 
+    [<CustomOperation("stdin")>]
+    member inline this.Stdin(attach) = this.Stdin(attach, true)
+
     [<CustomOperation("stdout")>]
     member inline _.Stdout(attach, stdout) = { attach with Stdout = stdout }
 
+    [<CustomOperation("stdout")>]
+    member inline this.Stdout(attach) = this.Stdout(attach, true)
+
     [<CustomOperation("stream")>]
     member inline _.Stream(attach, stream) = { attach with Stream = stream }
+
+    [<CustomOperation("stream")>]
+    member inline this.Stream(attach) = this.Stream(attach, true)
 
     member _.Yield(_: unit) = create id
 
