107ba400509b332cdb1187d7505789a0e034f7cc
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-07 11:58:03 -0500

    Refactor modules

--- src/FSharp/Container/Kill.fs
+++
@@ -1,7 +0,0 @@
-[<AutoOpen>]
-module UnMango.Docker.Container.Kill
-
-// https://docs.docker.com/engine/api/v1.41/#tag/Container/operation/ContainerKill
-type Kill = { Id: string; Signal: string option }
-
-let create id = { Id = id; Signal = None }
