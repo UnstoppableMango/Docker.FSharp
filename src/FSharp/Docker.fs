a1f2dd0200dbb0e3074173ce6f36ecf2f0c07993
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-06 23:42:45 -0500

    More operations

--- src/FSharp/Docker.fs
+++ src/FSharp/Docker.fs
@@ -9,7 +9,9 @@ type DockerBuilder() =
     member inline _.Combine(actions: Action list, xs: Action list) = actions @ xs
     member inline _.Delay(f) = f ()
     member inline _.Yield(container: Containers.Action) = [ Container container ]
+    member inline _.Yield(containers: Containers.Action list) = containers |> List.map Container
     member inline _.Yield(image: Images.Action) = [ Image image ]
-    member _.Zero() = []
+    member inline _.Yield(images: Images.Action list) = images |> List.map Image
+    member inline _.Zero() = []
 
 let docker = DockerBuilder()
