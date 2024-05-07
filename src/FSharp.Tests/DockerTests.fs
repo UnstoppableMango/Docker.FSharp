4a741d54e1d03fb12b454adaaa5abd5881701d60
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-07 17:03:37 -0500

    WIP on container propagation test

--- src/FSharp.Tests/DockerTests.fs
+++ src/FSharp.Tests/DockerTests.fs
@@ -1,6 +1,8 @@
 module UnMango.Docker.DockerTests
 
 open FsCheck.Xunit
+open UnMango.Docker.Containers
+open UnMango.Docker.Images
 
 [<Property>]
 let ``DockerBuilder builds container action`` (expected: Containers.Action) =
@@ -16,4 +18,15 @@ let ``DockerBuilder builds image action`` (expected: Images.Action) =
 
     match actual with
     | [ Image x ] -> expected = x
+    | _ -> false
+
+[<Property>]
+let ``DockerBuilder preconfigures container from image`` name =
+    let actual = docker {
+        Image.create { fromImage "" }
+        Container.create name { tty }
+    }
+
+    match actual with
+    | [ Container (Containers.Create x); _ ] -> x
     | _ -> false
