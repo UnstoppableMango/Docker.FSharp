130ddbd32a8505d58a0106d608f63024c3279ff9
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-07 20:12:36 -0500

    Enhanced DockerBuilder functionality
    Expanded the DockerBuilder's capabilities to handle various container actions such as attach, create, remove, start, stop and wait. Also added handling for image creation. This includes new tests to verify these functionalities. The changes provide a more robust and flexible way of managing docker containers and images in F#.

--- src/FSharp/Docker.fs
+++ src/FSharp/Docker.fs
@@ -7,11 +7,38 @@ type Action =
 
 type DockerBuilder() =
     member inline _.Combine(actions: Action list, xs: Action list) = actions @ xs
+
     member inline _.Delay(f) = f ()
+
     member inline _.Yield(container: Containers.Action) = [ Container container ]
+
     member inline _.Yield(containers: Containers.Action list) = containers |> List.map Container
+
+    member inline _.Yield(builder: Containers.AttachBuilder) =
+        [ builder.Yield() |> builder.Run |> Container ]
+
+    member inline _.Yield(builder: Containers.CreateBuilder) =
+        [ builder.Yield() |> builder.Run |> Container ]
+
+    member inline _.Yield(builder: Containers.RemoveBuilder) =
+        [ builder.Yield() |> builder.Run |> Container ]
+
+    member inline _.Yield(builder: Containers.StartBuilder) =
+        [ builder.Yield() |> builder.Run |> Container ]
+
+    member inline _.Yield(builder: Containers.StopBuilder) =
+        [ builder.Yield() |> builder.Run |> Container ]
+
+    member inline _.Yield(builder: Containers.WaitBuilder) =
+        [ builder.Yield() |> builder.Run |> Container ]
+
     member inline _.Yield(image: Images.Action) = [ Image image ]
+
     member inline _.Yield(images: Images.Action list) = images |> List.map Image
+
+    member inline _.Yield(builder: Images.CreateBuilder) =
+        [ builder.Yield() |> builder.Run |> Image ]
+
     member inline _.Zero() = []
 
 let docker = DockerBuilder()
