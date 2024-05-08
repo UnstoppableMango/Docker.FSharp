9634be4be870af74d96e10b9eb73680f295e3908
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-07 20:31:25 -0500

    Support "empty" container and image builders (#12)
    * WIP on container propagation test
    
    * Refactor Docker test for clarity
    
    Simplified the matching logic in DockerBuilder test. Replaced match statement with a direct list assignment to improve readability and maintainability.
    
    * Refactor Docker test assertions
    
    Simplified the assertion logic in Docker tests. Replaced match expressions with direct list comparisons for better readability and simplicity.
    
    * Enhanced DockerBuilder functionality
    
    Expanded the DockerBuilder's capabilities to handle various container actions such as attach, create, remove, start, stop and wait. Also added handling for image creation. This includes new tests to verify these functionalities. The changes provide a more robust and flexible way of managing docker containers and images in F#.
    
    * Vain attempt at increasing coverage

--- src/FSharp.Tests/DockerTests.fs
+++ src/FSharp.Tests/DockerTests.fs
@@ -1,19 +1,79 @@
 module UnMango.Docker.DockerTests
 
 open FsCheck.Xunit
+open UnMango.Docker.Containers
+open UnMango.Docker.Images
+
+[<Property>]
+let ``DockerBuilder Zero is empty`` () =
+    let actual = docker.Zero()
+
+    [] = actual
 
 [<Property>]
 let ``DockerBuilder builds container action`` (expected: Containers.Action) =
     let actual = docker { expected }
 
-    match actual with
-    | [ Container x ] -> expected = x
-    | _ -> false
+    [ Container expected ] = actual
 
 [<Property>]
 let ``DockerBuilder builds image action`` (expected: Images.Action) =
     let actual = docker { expected }
 
-    match actual with
-    | [ Image x ] -> expected = x
-    | _ -> false
+    [ Image expected ] = actual
+
+[<Property>]
+let ``DockerBuilder handles empty image create`` () =
+    let expected = Create.empty |> Action.Create
+
+    let actual = docker { Image.create }
+
+    [ Image expected ] = actual
+
+[<Property>]
+let ``DockerBuilder handles empty container attach`` id =
+    let expected = Attach.init id |> Containers.Action.Attach
+
+    let actual = docker { Container.attach id }
+
+    [ Container expected ] = actual
+
+[<Property>]
+let ``DockerBuilder handles empty container create`` id =
+    let expected = Create.init id |> Containers.Action.Create
+
+    let actual = docker { Container.create id }
+
+    [ Container expected ] = actual
+
+[<Property>]
+let ``DockerBuilder handles empty container remove`` id =
+    let expected = Remove.init id |> Containers.Action.Remove
+
+    let actual = docker { Container.remove id }
+
+    [ Container expected ] = actual
+
+[<Property>]
+let ``DockerBuilder handles empty container start`` id =
+    let expected = Start.init id |> Containers.Action.Start
+
+    let actual = docker { Container.start id }
+
+    [ Container expected ] = actual
+
+[<Property>]
+let ``DockerBuilder handles empty container stop`` id =
+    let expected = Stop.init id |> Containers.Action.Stop
+
+    let actual = docker { Container.stop id }
+
+    [ Container expected ] = actual
+
+[<Property>]
+let ``DockerBuilder handles empty container wait`` id =
+    let expected = Wait.init id |> Containers.Action.Wait
+
+    let actual = docker { Container.wait id }
+
+    [ Container expected ] = actual
